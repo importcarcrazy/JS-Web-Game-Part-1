b54875587641d4e259bc0afd9ec5336d057f8442
importcarcrazy <92552458+importcarcrazy@users.noreply.github.com>
2022-01-17 18:53:08 -0600

    Add files via upload
    Variations of the event listener are remarked.

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+let asset = document.createElement('img')
+asset.src = url
+asset.style.position = 'fixed'
+asset.style.left = left+'px'
+asset.style.bottom = bottom+'px'
+document.body.append(asset)
+}
+
+function newItem(url, left, bottom){
+    newImage(url, left, bottom)
+}
+
+/*function newItem.remove(){
+    newItem.addEventListener('click')
+}*/
+
+/*function newItem.remove(){
+    addEventListener('click')
+}*/
+
+/*function newItem.addEventListener('click'){
+    newItem.remove()
+}*/
+
+/*newItem.addEventListener('click', function(){ newItem.remove()}) */
+
+newImage('assets/green-character.gif' , 100,100)
+newImage('assets/pine-tree.png' , 450,200)
+newImage('assets/tree.png' , 200,300)
+newImage('assets/pillar.png' , 350,100)
+newImage('assets/crate.png' , 150,200)
+newImage('assets/well.png' , 500,425)
+
+newItem('assets/sword.png' , 500,405)
+newItem('assets/sheild.png' , 165,185)
+newItem('assets/staff.png' , 600,100)
+
